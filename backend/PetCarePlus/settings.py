eabc57e27fd8fbb0cb975836aa4c648db9063919
Rocky Chowdhury <rocky20809@gmail.com>
2026-02-06 04:41:57 +0600

    debuging in localhost

--- backend/PetCarePlus/settings.py
+++ backend/PetCarePlus/settings.py
@@ -107,6 +107,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
