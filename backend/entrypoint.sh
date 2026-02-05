96d5a9b40b4e6ad540871f8abdf87198b289b7a6
Rocky Chowdhury <rocky20809@gmail.com>
2026-02-06 03:33:31 +0600

    fix: make entrypoint script flexible for celery worker

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
-echo "Starting Gunicorn..."
-exec gunicorn PetCarePlus.wsgi:application --bind 0.0.0.0:8000
+if [ "$#" -gt 0 ]; then
+    exec "$@"
+else
+    echo "Starting Gunicorn..."
+    exec gunicorn PetCarePlus.wsgi:application --bind 0.0.0.0:8000
+fi
