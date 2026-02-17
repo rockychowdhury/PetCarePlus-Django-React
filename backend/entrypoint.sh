ae812e1640c5ca411cebd5351cd1220485d1f49d
Rocky Chowdhury <rocky20809@gmail.com>
2026-02-17 09:38:23 +0600

    deploye issue fixed

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Running migrations..."
+python manage.py migrate --noinput
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
@@ -8,5 +11,5 @@ if [ "$#" -gt 0 ]; then
     exec "$@"
 else
     echo "Starting Gunicorn..."
-    exec gunicorn PetCarePlus.wsgi:application --bind 0.0.0.0:8000
+    exec gunicorn PetCarePlus.wsgi:application --bind 0.0.0.0:${PORT:-8000}
 fi
