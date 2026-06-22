185ce24579a90ae3889422f9883518820b323df8
Rocky Chowdhury <rocky20809@gmail.com>
2026-06-22 15:38:45 +0600

    Fix django_q duplicate column error on render

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+python manage.py migrate django_q --fake
 python manage.py migrate
 python manage.py createcachetable || true
