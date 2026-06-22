e569bff3652bf4f755ac3b883715169862d01e49
Rocky Chowdhury <rocky20809@gmail.com>
2026-06-22 15:57:54 +0600

    Configure frontend proxies to use Render API

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api/v1': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://petcareplus-django-backned.onrender.com',
         changeOrigin: true,
       },
     },
